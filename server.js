fa8818c0ce798f3e2fa7ebfc9377deac645364b9
ashikurrahmans <ashiqur.dream@gmail.com>
2022-11-29 13:01:16 +0600

    category section done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = express()
 const cors = require("cors");
 port = process.env.PORT || 5000
-const ObjectId = require('mongodb').ObjectId;
+var ObjectId = require('mongodb').ObjectId
 
 app.use(cors());
 app.use(express.json());
@@ -13,36 +13,77 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.stqo7hs.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// Blog Collection 
 const dbName = process.env.DB_NAME;
-const blogCollec = process.env.Bio_Collect;
+const blogCollec = process.env.Blog_Collect;
+const usersCollec = process.env.usersCollect;
+
+
+// Category Collection 
+const categoryCollection = process.env.CategoryCol;
+
 
 
 async function run() {
   try {
     await client.connect();
-    const blogCollections = client
-    .db(dbName)
-    .collection(blogCollec);
+    const blogCollections = client.db(dbName).collection(blogCollec);
+    const usersCollections = client.db(dbName).collection(usersCollec);
+    const categories = client.db(dbName).collection(categoryCollection);
 
+
+
+// Home Route 
 
     app.get('/',(req,res)=>{
       res.send("This server is running")
     })
-    // Get All Blogs 
+
+ // Check Admin
+    app.get('/hello/dashboards/login',(req,res)=>{
+      const data = req.body;
+      console.log(data)
+
+    })
+
+
+    // Get All Blogs
 
     app.get("/blogs", async (req, res) => {
       const allblogs = await blogCollections.find({}).toArray();
       res.send(allblogs);
-      console.log(allblogs)
     });
 
-    
+
+
+   // Show single page blog 
+
     app.get('/blogs/:id', async (req, res) => {
       const id = req.params;
       const query = { _id: ObjectId(id) };
       const result = await blogCollections.findOne(query);
       res.send(result);
     });
+
+  
+// Show Categories 
+
+
+app.get('/categories',async (req,res)=>{
+  const allCategories = await categories.find({}).toArray();
+  res.send(allCategories);
+})
+
+
+
+    // Dashbord Add new post 
+
+    app.post(("/addnewarticle",(req,res)=>{
+      const data = req.body
+      console.log(data)
+    }))
+  
+  
 
 
   } catch {}
